b1c6e3696330ab46b2fa8d47214f5a34b337d138
Jamshidbek <jamanu1910077@gmail.com>
2023-11-20 14:05:44 +0500

    post new car handled

--- route/Car.js
+++ route/Car.js
@@ -14,6 +14,27 @@ route.get('/get_all', async (req, res) => {
 route.post('/addNew', async (req, res) => {
   const { model, picture, blurb, engine } = req.body;
 
-  res.send('working');
+  try {
+    // Check if required fields are present
+    if (!model || !picture || !blurb || !engine) {
+      return res.status(400).json({ error: 'Please provide all required fields.' });
+    }
+
+    // Create a new car instance
+    const newCar = new Cars({
+      model,
+      picture,
+      blurb,
+      engine,
+    });
+
+    // Save the new car to the database
+    const savedCar = await newCar.save();
+
+    res.status(201).json({ message: 'Car added successfully!', car: savedCar });
+  } catch (err) {
+    console.error('Error while adding car:', err);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
 });
 export default route;
